fd58302eea1e01b4edbb2b909eaf1ae53aba6563
linear <lineark2m@gmail.com>
2025-05-14 17:19:09 +0900

    Update MenuView.java

--- CafeKiosk/src/view/MenuView.java
+++ CafeKiosk/src/view/MenuView.java
@@ -1,16 +1,1 @@
 package view;
-
-import javax.swing.*;
-import java.awt.*;
-import controller.AppController;
-
-public class MenuView extends JFrame {
-    private AppController controller;
-
-    public MenuView(AppController controller) {
-        this.controller = controller;
-        setTitle("MenuView");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
