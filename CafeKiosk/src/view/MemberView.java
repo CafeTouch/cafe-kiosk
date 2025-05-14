f7a4bd6779c022ee8d176a7dd934007064149ffc
linear <lineark2m@gmail.com>
2025-05-14 17:18:42 +0900

    Update MemberView.java

--- CafeKiosk/src/view/MemberView.java
+++ CafeKiosk/src/view/MemberView.java
@@ -1,16 +1,1 @@
 package view;
-
-import javax.swing.*;
-import java.awt.*;
-import controller.AppController;
-
-public class MemberView extends JFrame {
-    private AppController controller;
-
-    public MemberView(AppController controller) {
-        this.controller = controller;
-        setTitle("MemberView");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
