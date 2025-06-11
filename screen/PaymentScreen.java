90c0f5c5b0fef54d131b236459bafda9dd58fd6c
linear <lineark2m@gmail.com>
2025-06-11 13:40:41 +0900

    Update PaymentScreen.java

--- screen/PaymentScreen.java
+++ screen/PaymentScreen.java
@@ -8,11 +8,11 @@ import java.awt.*;
 public class PaymentScreen extends JFrame {
     public PaymentScreen(AppController controller) {
         setTitle("결제 완료");
-        setSize(800, 400);
+        setSize(600, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("💳 결제가 완료되었습니다!", SwingConstants.CENTER);
+        JLabel label = new JLabel("결제가 완료되었습니다!", SwingConstants.CENTER);
         label.setFont(new Font("SansSerif", Font.BOLD, 40));
         add(label, BorderLayout.CENTER);
 
