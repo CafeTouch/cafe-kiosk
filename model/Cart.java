a5ed35b23e4035b21dcbdeffc2ef0d5a808c9d2d
jeemeez <jeemeez00@naver.com>
2025-06-13 05:04:53 +0900

    Update Cart.java

--- model/Cart.java
+++ model/Cart.java
@@ -1,28 +1,24 @@
 // [2] cart 관리 기능
 package model;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cart {
-    CartItem item;
-    List<CartItem> items = new ArrayList<CartItem>(); // 원소 = 장바구니 항목
+    private List<CartItem> items = new ArrayList<>(); // 원소 = 장바구니 항목
 
     public void addItem(CartItem item) { items.add(item); }
+
+    public List<CartItem> getItems() { return items; }
 
     public void deleteItem(CartItem item) {
         items.remove(item);
     }
 
-    public void increaseQuantity(CartItem item) {
-         item.quantity = item.getQuantity() + 1;
-    }
+    public void increaseQuantity(CartItem item) { item.quantity = item.getQuantity() + 1; }
 
-    public void decreaseQuantity(CartItem item) {
-        item.quantity = item.getQuantity() - 1;
-    }
+    public void decreaseQuantity(CartItem item) { item.quantity = item.getQuantity() - 1; }
 
-    public boolean isEmpty() {
-        return items.isEmpty();
-    } // 장바구니 비어있으면 결제하기 안 넘어가게 함
+    public boolean isEmpty() { return items.isEmpty(); } // 장바구니 비어있으면 결제하기 안 넘어가게 함
 
     public int getTotalCost() { // 총 금액
         int totalPrice = 0;
@@ -31,5 +27,15 @@ public class Cart {
         }
         return totalPrice;
     }
+    public void addOrUpdateItem(CartItem newItem) {
+        for (CartItem item : items) {
+            if (item.equals(newItem)) {
+                item.setQuantity(item.getQuantity() + newItem.getQuantity());
+                return;
+            }
+        }
+        items.add(newItem);
+    }
+
 
 }
