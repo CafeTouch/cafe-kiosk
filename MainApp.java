4a3cc112aff0b21df8789f83efca172f0c624d0c
Riwon24 <oatmeal24@sookmyung.ac.kr>
2025-06-14 03:12:03 +0900

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -2,6 +2,7 @@ import controller.AppController;
 
 public class MainApp {
     public static void main(String[] args) {
-        new AppController().startApp();
+        // 앱 첫 화면 실행, 나머지 진행은 컨트롤러에서
+        new AppController().newScreen();
     }
 }
