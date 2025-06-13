ed505ddb8cce4adafe205a6ba95acf333ebce6c8
Riwon24 <oatmeal24@sookmyung.ac.kr>
2025-06-14 03:05:38 +0900

    Update AppController.java

--- controller/AppController.java
+++ controller/AppController.java
@@ -4,29 +4,28 @@ import screen.*;
 import model.*;
 
 public class AppController {
+    // 컨트롤러에서 카트를 생성해서 다른 클래스에서 쭉 사용
     private Cart myCart = new Cart();
 
-    // 다른 클래스에서 모두 myCart 쓸 수 있또록
+    // 다른 클래스에서 모두 myCart 받아와 쓸 수 있도록 설정
     public Cart getCart() { return myCart;}
 
+    public void newScreen() { new FirstScreen(this);} // 초기화면(매장/포장)
 
-
-    public void newScreen() { new FirstScreen(this);}
-
-    public void startApp() {new MainFrame(this);}
-
-    //public void showCart() { new CartPanel(this,myCart);}
+    public void startApp() {new MainFrame(this);}// 본격적인 앱 시작(메인프레임)
 
     public void showCheckPurchaseScreen() {
-        new CheckPurchase(this, myCart);}
+        new CheckPurchase(this, myCart);} //
 
     public void showPaymentScreen() {
-        new PaymentScreen(this); // 결제 화면
+        new PaymentOptionScreen(this); // 결제 화면
     }
 
     public void showKeypadScreen() {
         new KeypadScreen(this);
     }
+
+    public void showPurchaseDone() { new PaymentScreen(this);}
 }
 
 
