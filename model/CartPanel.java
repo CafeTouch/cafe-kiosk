a0a77bd64119cfe053c940cedfa6cd15dfaababa
jeemeez <jeemeez00@naver.com>
2025-06-14 12:06:50 +0900

    Update CartPanel.java

--- model/CartPanel.java
+++ model/CartPanel.java
@@ -10,123 +10,143 @@ package model;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
-import controller.AppController;
 
 
 // 추후 메인프레임 하단 (SOUTH) 영역에 붙여질 예정
 public class CartPanel extends JPanel {
     public Cart cart; // 생성자
-    private CartItem item; // 생성자
     private JPanel mediumPanel;
-    private AppController controller;
+    private JLabel totalPriceLabel;// 총 금액
+    private JLabel totalQuantityLabel;
 
-
-    //public JPanel mediumPanel;
-    private JLabel priceLabel; // 단일 품목
-    private JLabel quantityLabel; // 수량
     Font font = new Font("맑은 고딕", Font.BOLD, 12);
-
-    public CartPanel(AppController controller,Cart cart) {
+    
+    // CART PANEL 객체 생성
+    public CartPanel(Cart cart) {
         this.cart = cart;
-        this.controller=controller;
 
+        // mediumPanel 속성
         this.mediumPanel = new JPanel();
         mediumPanel.setLayout(new BoxLayout(mediumPanel, BoxLayout.Y_AXIS));
-        mediumPanel.setBackground(new Color(200, 200, 255));
+        mediumPanel.setBackground(new Color(242, 242, 246));
         mediumPanel.setOpaque(true);
         mediumPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         mediumPanel.setMaximumSize(new Dimension(490, Integer.MAX_VALUE));
 
-        // 객체 리스트
+        JPanel pricePanel = createPricePanel();
+
+        // 리스트 items의 객체 item을 small Panel로 생성
         List<CartItem> items = cart.getItems();
         for (CartItem item : items) {
             mediumPanel.add(createSmallPanel(item));
         }
 
+        // BIG PANEL 속성
         JPanel bigPanel = new JPanel(new BorderLayout());
         bigPanel.setPreferredSize(new Dimension(600,200));
         bigPanel.setBackground(Color.WHITE);
 
+        // SCROLL PANE 속성: scrollPane이 mediumPanel 감싸는 구조
         JScrollPane scrollPane = new JScrollPane(mediumPanel);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         bigPanel.add(scrollPane, BorderLayout.CENTER);
 
-        JPanel pricePanel = createPricePanel();
+        // PRICE PANEL 속성: 금액, 결제버튼 담는 패널
         pricePanel.setPreferredSize(new Dimension(100, 200));
         bigPanel.add(pricePanel, BorderLayout.EAST);
-
+        
+        // BIG PANEL 배치
         add(bigPanel, BorderLayout.CENTER);
         bigPanel.setOpaque(true);
     }
 
-    // 결제창 버튼
+    // PAYMENT BUTTON: 결제창 버튼 (아직 미완성)
     public JButton paymentButton() {
+        // 1. 디자인 속성
         JButton payButton = new JButton("결제");
         payButton.setFont(font);
         payButton.setForeground(Color.WHITE);
+        // 2. 기능 속성
         payButton.addActionListener(e -> {
-            // 결제확인 누르면 결제 확인창으로 넘어감
-            controller.showCheckPurchaseScreen();
+            // 결제창으로 넘어가기
         });
         return payButton;
     }
-
-    // 수량 감소 버튼
-    private JButton decreaseButton(CartItem item, JLabel quantityLabel) {
-        JButton minusButton = new JButton("-"); // 감소 버튼
-        minusButton.setFont(new Font("맑은 고딕", Font.BOLD, 13));
-        minusButton.setMargin(new Insets(0, 0, 0, 0));
-        minusButton.setPreferredSize(new Dimension(30, 25));
-
-        minusButton.setForeground(Color.WHITE);
-        minusButton.addActionListener(e -> {
-            cart.decreaseQuantity(item);
-            if (item.getQuantity() <= 0) {
-                cart.deleteItem(item);
-                quantityLabel.setText("0");}
-            else {
-                quantityLabel.setText(String.valueOf(item.getQuantity()));
-            }
-            priceLabel.setText("총 금액: " + cart.getTotalCost() + "원");
-        });
-        return minusButton;
-    }
-
-    // 수량 증가 버튼
-    private JButton increaseButton(CartItem item, JLabel quantityLabel) {
+    
+    /* ----------------------------------------------------------------------------------------
+        BUTTON 생성 -> PANEL 생성 및 관리
+   -------------------------------------------------------------------------------------------- */
+    // INCREASE BUTTON: 수량 증가 버튼q
+    private JButton increaseButton(CartItem item, JLabel quantityLabel, JLabel singleTotalLabel) {
+        // 1. 버튼 디자인 속성
         JButton plusButton = new JButton("+"); // 증가 버튼
         plusButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
         plusButton.setMargin(new Insets(0, 0, 0, 0));
         plusButton.setPreferredSize(new Dimension(30, 25));
         plusButton.setForeground(Color.WHITE);
-        // 수량 증가 버튼
+        // 2. 버튼 기능 속성: 수량 +, singleTotalLabel +, totalPriceLabel +
         plusButton.addActionListener(e-> {
             {
                 cart.increaseQuantity(item);
                 quantityLabel.setText(String.valueOf(item.getQuantity()));
-                //priceLabel.setText("총 금액: " + cart.getTotalCost() + "원");
-                priceLabel = new JLabel("총 금액: " + cart.getTotalCost() + "원");
+                singleTotalLabel.setText(String.valueOf(item.getItemPrice()));
+                totalPriceLabel.setText(String.valueOf(cart.getTotalCost()) + "원");
+                refresh();
             }
-
         });
         return plusButton;
     }
 
-    // item 패널 생성
+    // DECREASE BUTTON: 수량 증가 버튼
+    private JButton decreaseButton(CartItem item, JLabel quantityLabel, JLabel singleTotalLabel) {
+        // 1. 버튼 디자인 속성
+        JButton minusButton = new JButton("-"); // 감소 버튼
+        minusButton.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+        minusButton.setMargin(new Insets(0, 0, 0, 0));
+        minusButton.setPreferredSize(new Dimension(30, 25));
+        minusButton.setForeground(Color.WHITE);
+        // 2. 버튼 기능 속성: 수량 -, singleTotalLabel -, totalPriceLabel -
+        minusButton.addActionListener(e -> {
+            if (item.getQuantity() == 1) {
+                int result = JOptionPane.showConfirmDialog(null,
+                        "해당 메뉴를 삭제하시겠습니까?",
+                        " 삭제 확인", JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    cart.deleteItem(item);
+                    refresh();
+                    return;
+                } else { item.setQuantity(1); }
+            }
+            else {
+                cart.decreaseQuantity(item);
+            }
+            quantityLabel.setText(String.valueOf(item.getQuantity()));
+            singleTotalLabel.setText(String.valueOf(item.getItemPrice()));
+            totalPriceLabel.setText(String.valueOf(cart.getTotalCost()) + "원");
+            refresh();
+        });
+        return minusButton;
+    }
+
+
+    // SMALL PANEL 생성: 메뉴 1개 -> ui 구현
     public JPanel createSmallPanel(CartItem item) { // top + bottom 패널 = medium panel
+        // 1. SMALL PANEL 디자인 속성
         JPanel smallPanel = new JPanel(new GridLayout(2, 1));
         smallPanel.setLayout(new BoxLayout(smallPanel, BoxLayout.Y_AXIS));
         smallPanel.setBackground(Color.WHITE);
         smallPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
-        //top panel
+        // 1-1. TOP PANEL
         JPanel topPanel = new JPanel(new BorderLayout()); // topPanel: 메뉴명, 수량, 가격
         topPanel.setBackground(new Color(200, 220, 255));
 
+        // 1-1-A. NAMEHOT PANEL 디자인 속성: 이름과 온도를 TOP PANEL 맨 왼쪽에 배치
         JPanel nameHotPanel = new JPanel(new BorderLayout());
         nameHotPanel.setMaximumSize(new Dimension(450, 40));
 
+        // 1-1-A-1. HOT PANEL 디자인 및 기능 속성 -> NAMEHOT PANEL에 ADD
         JLabel hotLabel;
         if (item.getHot() == 1) {
             hotLabel = new JLabel("HOT");
@@ -149,30 +169,41 @@ public class CartPanel extends JPanel {
         hotLabel.setOpaque(true);
         nameHotPanel.add(hotLabel, BorderLayout.WEST);
 
-        // 1. 메뉴명 nameLabel
+        // 1-1-A-2. NAME PANEL 디자인 및 기능 속성 -> NAMEHOT PANEL에  ADD
         JLabel nameLabel = new JLabel(" " + item.getName(), SwingConstants.CENTER); // 1. 메뉴명
         nameLabel.setBackground((new Color(184,216,249)));
         nameLabel.setPreferredSize(new Dimension(80, 40));
         nameLabel.setOpaque(true);
         nameHotPanel.add(nameLabel);
 
+        // TOP PANEL에 NAMEHOT PANEL 배치
         topPanel.add(nameHotPanel, BorderLayout.WEST);
 
-        // 2. 수량 조절 quantityLabel -> quantityPanel
-        JPanel quantityPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        // 1-1-B. QUANTITY PANEL
+        JPanel quantityPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 5));
+        quantityPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
 
+        // 1-1-B-1. QUANTITY LABEL 속성:
         JLabel quantityLabel = new JLabel(String.valueOf(item.getQuantity()), SwingConstants.CENTER);
-        quantityLabel.setBackground(Color.RED);
-
-        quantityLabel.setPreferredSize(new Dimension(40, 20));
+        quantityLabel.setBackground(new Color(242, 242, 246));
+        quantityLabel.setOpaque(true);
+        quantityLabel.setMinimumSize(new Dimension(60, 25));
+        quantityLabel.setPreferredSize(new Dimension(60, 25));
+        quantityLabel.setMaximumSize(new Dimension(60, 25));
+        quantityLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // 3. 수량 조절 버튼 속성
-        JButton minusButton = decreaseButton(item, quantityLabel); // 감소 버튼
+        JLabel singleTotalLabel = new JLabel(" 가격: " + item.getItemPrice() + "원 ");
+        singleTotalLabel.setBackground(new Color(200,220,255));
+        singleTotalLabel.setSize(60, 80);
+        singleTotalLabel.setOpaque(true);
+
+        JButton minusButton = decreaseButton(item, quantityLabel, singleTotalLabel); // 감소 // 버튼
         minusButton.setPreferredSize(new Dimension(25, 25));
         minusButton.setBackground(new Color(0,102,204));
         minusButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
-        JButton plusButton = increaseButton(item, quantityLabel); // 증가 버튼
+        JButton plusButton = increaseButton(item, quantityLabel, singleTotalLabel); // 증가 버튼
         plusButton.setPreferredSize(new Dimension(25, 25));
         plusButton.setBackground(new Color(0,102,204));
         plusButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
@@ -181,14 +212,10 @@ public class CartPanel extends JPanel {
         quantityPanel.add(minusButton); // - 버튼
         quantityPanel.add(quantityLabel); // 수량 표시
         quantityPanel.add(plusButton); // + 버튼
+        quantityPanel.add(Box.createRigidArea(new Dimension(6, 0)));
         quantityPanel.setBackground(Color.WHITE);
         topPanel.add(quantityPanel, BorderLayout.CENTER);
 
-        // 3. 항목 당 가격
-        JLabel singleTotalLabel = new JLabel(" 가격: " + item.getItemPrice() + "원 ");
-        singleTotalLabel.setBackground(new Color(200,220,255));
-        singleTotalLabel.setSize(60, 80);
-        singleTotalLabel.setBackground(Color.RED);
         topPanel.add(singleTotalLabel, BorderLayout.EAST);
 
         // bottom Panel 속성 설정
@@ -233,8 +260,9 @@ public class CartPanel extends JPanel {
         topPanel.setMaximumSize(new Dimension(490, 40));
         bottomPanel.setMaximumSize(new Dimension(490, 25));
 
-        smallPanel.setMaximumSize(new Dimension(490, 100));
+        smallPanel.setMaximumSize(new Dimension(490, 65));
         smallPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        smallPanel.setPreferredSize(null); // 레이아웃에 맞게 자동
 
         smallPanel.add(topPanel);
         smallPanel.add(bottomPanel);
@@ -247,14 +275,24 @@ public class CartPanel extends JPanel {
 
     // 가격 버튼 있는 패널 생성
     public JPanel createPricePanel() { //top, bottom
+        totalPriceLabel = new JLabel(cart.getTotalCost() + "원");
+        totalPriceLabel.setBackground(new Color(200, 220, 255));
+        totalPriceLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+        totalPriceLabel.setOpaque(true);
+        totalPriceLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        totalQuantityLabel = new JLabel(cart.getTotalQuantity() + "개");
+        totalQuantityLabel.setBackground(new Color(200, 220, 255));
+        totalQuantityLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+        totalQuantityLabel.setOpaque(true);
+        totalQuantityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         JPanel pricePanel = new JPanel();
         pricePanel.setLayout(new BoxLayout(pricePanel, BoxLayout.Y_AXIS));
         pricePanel.setBackground(Color.WHITE);
 
-        JLabel priceLabel1 = new JLabel("주문 금액");
-        JLabel priceLabel2 = new JLabel(cart.getTotalCost() + "원");
-        priceLabel1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        priceLabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel priceTitleLabel = new JLabel("주문 금액");
+        priceTitleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JButton payButton = paymentButton();
         payButton.setPreferredSize(new Dimension(60, 20));
@@ -263,8 +301,8 @@ public class CartPanel extends JPanel {
         payButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         pricePanel.add(Box.createVerticalStrut(20));
-        pricePanel.add(priceLabel1);
-        pricePanel.add(priceLabel2);
+        pricePanel.add(priceTitleLabel);
+        pricePanel.add(totalPriceLabel);
         pricePanel.add(Box.createVerticalGlue());
         pricePanel.add(payButton);
         pricePanel.add(Box.createVerticalGlue());
@@ -272,21 +310,17 @@ public class CartPanel extends JPanel {
     }
 
     public void refresh() {
+        if (totalPriceLabel == null) { return; }
+
         mediumPanel.removeAll();             // 기존 UI 컴포넌트 제거
         List<CartItem> items = cart.getItems();  // 장바구니 현재 항목 가져오기
-
         for (CartItem item : items) {
             JPanel itemPanel = createSmallPanel(item);
             mediumPanel.add(itemPanel);
         }
+        totalPriceLabel.setText(cart.getTotalCost() + "원");
         mediumPanel.revalidate();  // 레이아웃 갱신
         mediumPanel.repaint();     // UI 다시 그림
     }
-
-    // 패널 삭제
-    public void deletePanel(JPanel mediumPanel, JPanel smallPanel) {
-        mediumPanel.remove(smallPanel);
-        mediumPanel.revalidate();
-        mediumPanel.repaint();}
 
 }
