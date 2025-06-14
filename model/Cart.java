e587c7e58b84686f11c6ec82bba6b15a997e8acc
jeemeez <jeemeez00@naver.com>
2025-06-14 19:20:22 +0900

    Update Cart.java
    주석 추가

--- model/Cart.java
+++ model/Cart.java
@@ -4,16 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    // items의 각 원소 = 항목 1개 (cartItem item)
+    // items의 각 원소 = 항목 객체 1개 (cartItem item)
     private List<CartItem> items = new ArrayList<>();
 
-    // 장바구니에 항목 추가
+    // 장바구니에 항목 객체 추가
     public void addItem(CartItem item) { items.add(item); }
 
-    // 장바구니에서 항목 삭제
-    public void deleteItem(CartItem item) {
-        items.remove(item);
-    }
+    // 장바구니에서 항목 객체 삭제
+    public void deleteItem(CartItem item) { items.remove(item); }
 
     // 장바구니에서 항목 get
     public List<CartItem> getItems() { return items; }
@@ -24,8 +22,10 @@ public class Cart {
     // 항목 객체의 수량 감소
     public void decreaseQuantity(CartItem item) { item.quantity = item.getQuantity() - 1; }
 
-    public boolean isEmpty() { return items.isEmpty(); } // 장바구니 비어있으면 결제하기 안 넘어가게 함
+    // 장바구니 리스트 공백 검사
+    public boolean isEmpty() { return items.isEmpty(); }
 
+    // 항목 객체들의 총 금액 합산
     public int getTotalCost() { // 총 금액
         int totalPrice = 0;
         for (CartItem item : items) {
@@ -33,7 +33,8 @@ public class Cart {
         }
         return totalPrice;
     }
-
+    
+    // 항목 객체의 총 개수 합산
     public int getTotalQuantity() { // 총 금액
         int totalQtt = 0;
         for (CartItem item : items) {
@@ -42,6 +43,7 @@ public class Cart {
         return totalQtt;
     }
 
+    
     public void addOrUpdateItem(CartItem newItem) {
         for (CartItem item : items) {
             if (item.equals(newItem)) {
