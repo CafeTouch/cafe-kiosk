ce100afda77295154cbec60dfc6c83b4041fc7a1
jeemeez <jeemeez00@naver.com>
2025-06-14 19:35:58 +0900

    Update CartItem.java

--- model/CartItem.java
+++ model/CartItem.java
@@ -1,55 +1,50 @@
-// [1] 단일 item 관리
+// 장바구니에 담을 항목 구성
 package model;
 import java.util.Objects;
 
-// 객체 hot, sweetener 추가했음
 public class CartItem {
     private String name; // 메뉴 이름
     private int price; // 단일 메뉴 가격
-    private int hot;
+    private int hot; // 온도
     private String size; // 사이즈
-    private int extraShot; // 0 또는 1
-    private int sweetener;
-    private int disposables; // 0 또는 1
-    public int quantity;
+    private int extraShot; // 1이면 추가 0이면 추가 X
+    private int sweetener; // 1이면 추가 0이면 추가 X
+    private int disposables; // 1이면 추가 0이면 추가 X
+    public int quantity; // 수량
     public CartItem() {}
 
     public CartItem(String name, int price, int hot, String size, int extraShot, int sweetener, int disposables, int quantity) {
         this.name = name;
         this.price = price;
-        this.hot = hot; // hot이면 1, ice면 0
-        this.size = size; // small or large
-        this.extraShot = extraShot; // 1이면 추가, 0이면 안 함
-        this.sweetener = sweetener; // ==
+        this.hot = hot;
+        this.size = size;
+        this.extraShot = extraShot;
+        this.sweetener = sweetener;
         this.disposables = disposables;
         this.quantity = quantity;
     }
 
-    public String getName() {
-        return name;
-    } // 메뉴명
+    // getter 설정
+    public String getName() { return name; } // 메뉴명
 
-    public int getPrice() {
-        return price;
-    } // 단일 가격
+    public int getPrice() { return price; } // 단일 가격
 
-    public int getHot() { return hot; }
+    public int getHot() { return hot; } // 온도: Hot or Iced
 
-    public String getSize() {
-        return size;
-    } // 사이즈: Small / Large
+    public String getSize() { return size; } // 사이즈: Small or Large
 
-    public int getDisposables() { return disposables; } // YES = 1 / No = 0
+    public int getDisposables() { return disposables; } // 일회용품 추가: YES = 1 / NO = 0
 
-    public int getExtraShot() { return extraShot; } // YES = 1 / No = 0
+    public int getExtraShot() { return extraShot; } // 샷 추가: YES = 1 / NO = 0
 
-    public int getSweetener() { return sweetener; }
+    public int getSweetener() { return sweetener; } // 시럽 추가: YES = 1 / NO = 0
 
-    public int getItemPrice() { return (price + extraShot * 500 + sweetener * 500) * quantity; } // 단일 item 가격 계산
+    public int getQuantity() { return quantity; } // 수량
 
-    public int getQuantity() { return quantity; }
+    public int getItemPrice() { return (price + extraShot * 500 + sweetener * 500) * quantity; } // 옵션 선택 후 변경 된 금액
 
-    // factory에서 사용
+
+    // setter 설정: factory에서 사용
 
     public void setName(String name) { this.name = name; }
 
