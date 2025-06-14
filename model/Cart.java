c1278c61e13a0b8cb13183f707e1fc5eb9bd9ae4
jeemeez <jeemeez00@naver.com>
2025-06-14 19:16:52 +0900

    Update Cart.java
    주석 추가

--- model/Cart.java
+++ model/Cart.java
@@ -1,21 +1,27 @@
-// [2] cart 관리 기능
+// 장바구니 관리 기능
 package model;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private List<CartItem> items = new ArrayList<>(); // 원소 = 장바구니 항목
+    // items의 각 원소 = 항목 1개 (cartItem item)
+    private List<CartItem> items = new ArrayList<>();
 
+    // 장바구니에 항목 추가
     public void addItem(CartItem item) { items.add(item); }
 
-    public List<CartItem> getItems() { return items; }
-
+    // 장바구니에서 항목 삭제
     public void deleteItem(CartItem item) {
         items.remove(item);
     }
 
+    // 장바구니에서 항목 get
+    public List<CartItem> getItems() { return items; }
+
+    // 항목 객체의 수량 증가
     public void increaseQuantity(CartItem item) { item.quantity = item.getQuantity() + 1; }
 
+    // 항목 객체의 수량 감소
     public void decreaseQuantity(CartItem item) { item.quantity = item.getQuantity() - 1; }
 
     public boolean isEmpty() { return items.isEmpty(); } // 장바구니 비어있으면 결제하기 안 넘어가게 함
